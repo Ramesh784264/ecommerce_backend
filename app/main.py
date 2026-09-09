7840594bf616a9fcbbfe876696db0e8c78ca7188
Ramesh784264 <gramesh192004@gamil.com>
2026-09-10 01:27:02 +0530

    updated main py configuration

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
 from app.config.database import Base, engine
-from app.routes import auth, category, product, vendor, admin, cart
-from app.models import category_model, product_model, vendor_model, cart_model
+from app.models import (
+    user_model,
+    category_model,
+    product_model,
+    vendor_model,
+    cart_model,
+    wishlist_model,
+)
+from app.routes import auth, category, product, vendor, admin, cart, wishlist
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="E-Commerce API")
-
 
 app.include_router(auth.router)
 app.include_router(category.router)
@@ -14,6 +20,7 @@ app.include_router(product.router)
 app.include_router(vendor.router)
 app.include_router(admin.router)
 app.include_router(cart.router)
+app.include_router(wishlist.router)
 
 
 @app.get("/")
