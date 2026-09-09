8436b7cf8015ce2cf7a19a4b6e1aa3bfc6c2bd22
Ramesh784264 <gramesh192004@gamil.com>
2026-09-10 04:19:09 +0530

    updated category py  configuration

--- app/routes/category.py
+++ app/routes/category.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from app.config.database import get_db
 from app.models.category_model import Category
-from app.schemas.category_schema import CategoryCreate, CategoryResponse
+from app.schemas.category_schema import CategoryCreate, CategoryUpdate, CategoryResponse
 from app.utils.dependencies import role_required
 
 router = APIRouter(prefix="/categories", tags=["Categories"])
@@ -31,6 +31,37 @@ def create_category(
 @router.get("/", response_model=list[CategoryResponse])
 def get_categories(db: Session = Depends(get_db)):
     return db.query(Category).all()
+
+
+# ---------------- UPDATE (Admin only) ----------------
+@router.put("/{category_id}", response_model=CategoryResponse)
+def update_category(
+    category_id: int,
+    data: CategoryUpdate,
+    db: Session = Depends(get_db),
+    current_user=Depends(role_required("admin")),
+):
+    category = db.query(Category).filter(Category.id == category_id).first()
+    if not category:
+        raise HTTPException(
+            status_code=404, detail=f"Category not found with this id {category_id}"
+        )
+
+    # Vera category same name use pannuthaa nu check pannuvom (name update pannina)
+    if data.name and data.name != category.name:
+        name_exists = db.query(Category).filter(Category.name == data.name).first()
+        if name_exists:
+            raise HTTPException(
+                status_code=400, detail="Category with this name already exists"
+            )
+
+    update_data = data.dict(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(category, key, value)
+
+    db.commit()
+    db.refresh(category)
+    return category
 
 
 # ---------------- DELETE (Admin only) ----------------
